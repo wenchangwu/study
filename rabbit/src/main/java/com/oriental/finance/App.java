7f19a74902193b35130c08b3f8682b35affd7ca5
xiao_dingo <wuwc@oriental-finance.com>
2017-06-20 22:47:59 +0800

    init

---
+++ rabbit/src/main/java/com/oriental/finance/App.java
@@ -0,0 +1,13 @@
+package com.oriental.finance;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
